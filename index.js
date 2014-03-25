ca956eb8cb8d985c41868ba771baf7079f59c2c5
Jesse Williamson <j.williamson.sd@gmail.com>
2014-03-25 15:29:00 -0700

    broken things into sections. Added numbers

--- index.js
+++ index.js
@@ -4,13 +4,19 @@
  */
 
 var λ = {};
+/**
+ * Foundations
+ * -----------
+ * 
+ * A few foundational functions.
+ */
 
 /**
  * λx.x
  *
  * The identity function. Given a value 'x', it will return that value,
 */
-λ.id = function id(x){
+λ.identity = function identity(x){
   return x;
 };
 
@@ -39,9 +45,8 @@ var λ = {};
  *
  * Given a value 'x', and a value 'y', returns 'x'.
  * This function can be used in pairs to return the first item.
- * It can also be used in conditionals (<expression> ? <true> : <false>), to represent true.
 */
-λ.truth = λ.first = function first(x){
+λ.first = function first(x){
   return function(y){
     return x;
   };
@@ -51,11 +56,11 @@ var λ = {};
  * λx.λy.y
  *
  * Given a value 'x' and a value 'y', returns 'y'.
- * This function is the inverse of λ.true (λ.first). It could be expressed in much the same way as the previous function.
+ * This function is the inverse of λ.first. It could be expressed in much the same way as the previous function.
  * However, the returned function is equivilant to the identity function, so simply returning λ.id is simpler.
  */
-λ.untruth = λ.second = function second(x){
-  return λ.id;
+λ.second = function second(x){
+  return λ.identity;
 };
 
 /**
@@ -66,13 +71,35 @@ var λ = {};
  * For example:
  * λ.pair(λ.id)(λ.apply)(λ.first) //=> λ.id
  */
-λ.cond = λ.pair = function pair(x){
+λ.pair = function pair(x){
   return function(y){
     return function(f){
       return f(x)(y);
     };
   };
 };
+
+/**
+ * Boolean Logic
+ * -------------
+ * Consider a conditional expression <condition> ? <true> : <false>
+ * This can be expressed as a pair, where the first value represents True, and the second represents false.
+ */
+
+/**
+ * In Boolean logic, a conditional can be expressed as a pair of expressions, with a condition for selecting the first or second.
+ */
+λ.cond = λ.pair;
+
+/**
+ * If a conditional expression can be expressed as a list, then True can be expressed as selecting the first item in the list.
+ */
+λ.truth = λ.first;
+
+/**
+ * If True is expressed as λ.first, then it follows that False can be expressed as selecting the second item.
+ */
+λ.untruth = λ.second;
 
 /**
  * λx.(((λ.cond λ.untruth) λ.truth) x)
@@ -112,6 +139,39 @@ var λ = {};
   return function(y){
     return x(λ.truth)(y);
   };
+};
+
+/**
+ * Numbers
+ * -------
+ * Natural numbers can be expressed as a number of successions from zero, and zero can be expressed as the identity function.
+ * and so on.
+ */
+λ.zero = λ.identity;
+
+/**
+ * λn.λs((λ.second) n)
+ *
+ * Given a number 'n', create a pair where λ.second is the first item and 'n' is the second. In this way, we can express numbers as such:
+ * one = λ.succ(λ.zero) == [λ.second, λ.zero]
+ * two = λ.succ(one) == [λ.second, one] == [λ.second, [λ.second, λ.zero]]
+ * And so on.
+ */
+λ.succ = function succ(n){
+  return λ.pair(λ.second)(n);
+};
+
+/**
+ * λn.(n λ.first)
+ *
+ * Given a number 'n', returns True if that number is zero, otherwise returns False.
+ *
+ * Since a number can be expressed as a pair, where the first item is λ.second, which is λ.untruth or False, we can select the first value from the pair
+ * to determine if the number is zero. When λ.first is applied to a number number, λ.untruth is returned. When λ.first is applied to λ.zero, which
+ * is λ.identity, it returns itself (which is the same as λ.truth or True)
+ */
+λ.iszero = function(n){
+  return n(λ.first);
 };
 
 module.exports = λ;
